f0a3f547721554ea5fa676c2d27e1879a49650fe
flopixx <flor.vel96@gmail.com>
2021-01-27 17:45:52 -0300

    termine

---
+++ README.md
@@ -0,0 +1,3 @@
+## Welcome! 👋
+HOLA LES DEJO LA DEMO DE MI LANDING PAGES
+https://cocky-kare-e62c91.netlify.app/
---
+++ app.js
@@ -0,0 +1,33 @@
+
+
+const navSile= ()=>{
+    const hambur= document.querySelector(".meni");
+    const nav=document.getElementById("nav")
+   
+  
+    
+    
+    hambur.addEventListener('click', ()=>{
+      
+      
+    nav.classList.toggle('active')
+
+    
+    
+    
+    
+    
+       
+    
+    
+    
+    
+    
+    
+        
+    })
+    }
+    
+        
+    navSile()
+ 
